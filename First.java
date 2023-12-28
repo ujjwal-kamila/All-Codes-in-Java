07509dc572bd10fb18a9130820328075229b0d11
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-28 21:12:01 +0530

    Java

---
+++ First.java
@@ -0,0 +1,12 @@
+//First code in Java
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("*");
+        System.out.println("**");
+        // Additional lines for asterisk pattern
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
