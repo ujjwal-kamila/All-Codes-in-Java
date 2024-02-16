86e2ac99d80c6fe1fddb73fe05633b84b03661d1
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-02-16 09:26:47 +0530

    First Program in Java

--- First.java
+++ First.java
@@ -1,12 +1,8 @@
-//First code in Java
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("*");
-        System.out.println("**");
-        // Additional lines for asterisk pattern
+        System.out.println("Hello World!");
+        System.out.println(67);
         System.out.println("***");
-        System.out.println("****");
     }
 }
