85d8fde3c9bc9c1840c3fce1a1b1ced0deeca48f
Ujjwal <ujjwalkamila86@gmail.com>
2024-04-22 22:27:45 +0530

    updated java codes

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello World!");
+        System.out.println("Ujjwal kamila");
         // System.out.println(args[1]);
     }
 }
