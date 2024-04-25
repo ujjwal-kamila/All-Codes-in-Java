6a731dce4beaecdcc0b4a5570c225d982079ad88
Ujjwal <ujjwalkamila86@gmail.com>
2024-04-25 20:03:16 +0530

    Java codes

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.next());
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
+    //psvm shortcut for main fun 
+    //  sysout shortcut for print
     public static void main (String[] args){
         System.out.println("Hello World!");
         System.out.println("Ujjwal kamila");
         // System.out.println(args[1]);
     }
+
 }
