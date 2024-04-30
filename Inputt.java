e1f1c4e4ed1151fe971510a0e7d5b54da675f52e
Ujjwal <ujjwalkamila86@gmail.com>
2024-04-30 20:16:26 +0530

    updated java codes

--- Inputt.java
+++ Inputt.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
+//string and int inputs
 public class Inputt {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // System.out.print("Enter some input :");
         // int roll_no = input.nextInt();
         // System.out.println("Your roll number is : " +roll_no);
-
         // int a = 234_000_000
         String name = input.next();
         System.out.println("Your Name is :  "+name);
