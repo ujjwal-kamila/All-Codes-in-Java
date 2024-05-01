168356df2d0d5eb048aa2f14b0f3e3c5377cfb41
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-01 19:19:59 +0530

    Java codes

--- Inputt.java
+++ Inputt.java
@@ -7,6 +7,7 @@ public class Inputt {
         // System.out.print("Enter some input :");
         // int roll_no = input.nextInt();
         // System.out.println("Your roll number is : " +roll_no);
+        
         // int a = 234_000_000
         String name = input.next();
         System.out.println("Your Name is :  "+name);
