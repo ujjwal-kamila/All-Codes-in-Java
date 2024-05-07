d9f96b69dbbd8c80088ef9f41f7018667d2b444f
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-07 18:12:05 +0530

    Java codes

--- Inputs.java
+++ Inputs.java
@@ -8,9 +8,14 @@ public class Inputs {
         // int roll_no = input.nextInt();
         // System.out.println("Your roll number is : " +roll_no);
         
+        //underscore(_) ignored with comma (,)
         // int a = 234_000_000
+        System.out.println("Enter Your name :");
         String name = input.next();
         System.out.println("Your Name is :  "+name);
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
 
 
     }
---
+++ Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first numbers : ");
+        int num1 = input.nextInt();
+        System.out.println("Enter Second numbers : ");
+        int num2 = input.nextInt();
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Print the sum
+        System.out.println("Sum : " + sum);
+
+
+        //using float
+        12.12
+    }
+}
+
