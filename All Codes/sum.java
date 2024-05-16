2bc437d7cef4fe618d81bb6af135d1047241796c
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-16 22:45:23 +0530

    Java codes

---
+++ All Codes/sum.java
@@ -0,0 +1,16 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println("Sum is : "+sum);
+
+        int mul = a * b  ;
+        System.out.println("Multiplication is : "+mul); 
+
+
+
+
+    }
+    
+}
---
+++ All Codes/variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main(String[] args) {
+        
+        //variables
+        String name = "ujjwal";
+        int age = 10;
+        float pi = 3.14;
+        double price0 = 25.25;
+        int a = 10;
+        int b = 5;
+        name = "Ujjwal";
+        
+    }
+    
+}
