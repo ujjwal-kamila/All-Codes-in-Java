f840f85a0620046a1d4b062801aef0ceadd9dba7
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-12-05 19:05:28 +0530

    Oops Lab Day -4 Codes

---
+++ Oops Lab Codes/Anagram.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Anagram{
+    public static void main(String[] args) {
+        String str1 = "Listen";
+        String str2 = "Silent";
+
+        
+        boolean isAnagram = checkAnagram(str1, str2);
+
+        
+        if (isAnagram) {
+            System.out.println("Yes, the strings are anagrams.");
+        } else {
+            System.out.println("No, the strings are not anagrams.");
+        }
+    }
+
+    public static boolean checkAnagram(String str1, String str2) {
+        
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+     
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
